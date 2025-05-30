e993f2005decfca98efd3ea0b0beea351c7114e5
Amber Wang <wangshengyan6@gmail.com>
2025-05-30 20:17:05 +0800

    chore: Home section layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,14 @@ const Home = () => {
   return (
     <section
       id="home"
-      className="w-full h-lvh bg-cover bg-center bg-fixed flex justify-center relative  "
+      className="w-full h-screen bg-cover bg-center bg-fixed flex justify-center relative  "
       style={{
         backgroundImage: "url('/public/images/background.jpeg')",
       }}
     >
       <div className="absolute inset-0 bg-black/50"></div>
-      <div className="home-container max-w-[70%] md:flex-row flex flex-col justify-center items-center mx-auto">
-        <div className="relative z-10 slogan mr-10 text-center text-white">
+      <div className="home-container max-w-[70%] md:flex-row flex flex-col mt-4 justify-center items-center mx-auto">
+        <div className="relative z-10 slogan text-center text-white">
           <h1 className="text-5xl font-bold ">Hi I'm Amber.</h1>
           <p className="text-2xl mt-5">
             Full-Stack Developer in Taiwan | React & JS
