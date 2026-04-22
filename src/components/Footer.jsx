304f522586d2e448881a8d4854c9168d3c204584
Amber Wang <wangshengyan6@gmail.com>
2026-04-22 17:11:24 +0800

    refactor: 整理contact區域
    - 刪除contact form
    - 刪除facebook和email連接
    - 改以陣列方式渲染鏈接

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     const year = new Date().getFullYear();
   return (
-    <footer className="py-10 flex justify-end text-sm font-semibold px-4 dark:bg-gray-800 dark:text-white">
+    <footer className="py-10 flex justify-end text-sm font-semibold px-4 bg-gray-200 dark:bg-black/90  dark:text-white">
       <p>Copyright© {year} Amber Wang. All rights reserved.</p>
     </footer>
   );
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -2,20 +2,40 @@ import React from "react";
 import ContactForm from "../components/ContactForm";
 import { FaFacebook, FaLinkedin, FaAt, FaGithub } from "react-icons/fa";
 const Contact = () => {
+  const contactInfo = [
+    { name: "Github", icon: FaGithub, href: "https://github.com/lixinjie520" },
+    {
+      name: "LinkedIn",
+      icon: FaLinkedin,
+      href: "https://www.linkedin.com/in/%E5%8B%9D%E7%87%95-%E7%8E%8B-61b6b3286/",
+    },
+  ];
   return (
     <section
       id="contact"
-      className="p-20 bg-gray-200 dark:bg-gray-700 dark:text-white"
+      className="py-18 px-6 dark:bg-gray-800 dark:text-white"
     >
-      <h2 className="text-4xl md:text-5xl pb-24 text-center font-bold">
+      <h2 className="text-4xl md:text-5xl mb-10 text-center font-bold">
         Let's work together...
       </h2>
-      <div className="grid grid-cols-1 lg:grid-cols-2 px-30 place-items-center">
-        {/* 左邊聯繫表單 */}
-        <ContactForm />
-        {/* 右邊聯繫區 */}
-        <div className="max-w-3xl md:max-w-5xl md:mt-10  mx-auto text-2xl font-bold grid grid-cols-1 gap-14 mt-10 place-items-center rounded">
-          <div className="contact-card">
+
+      <div className="flex px-30 items-center">
+        <div className="flex gap-20 max-w-full md:max-w-5xl mt-4 md:mt-6 mx-auto text-lg md:text-2xl font-bold rounded">
+          {contactInfo.map((con, index) => {
+            const Icon = con.icon;
+            return (
+              <div key={index} className="flex gap-4 hover:scale-105 transition duration-200 hover:text-amber-700">
+                <a
+                  href={con.href}
+                  target="_blank"
+                  className="flex items-center"
+                >
+                  <Icon className="mr-1"></Icon>
+                  {con.name}
+                </a>
+              </div>
+            ) })}
+          {/* <div className="contact-card">
             <a
               href="https://www.facebook.com/shengyan.wang1"
               target="_blank"
@@ -24,33 +44,33 @@ const Contact = () => {
               <FaFacebook className="mr-1"></FaFacebook>
               Facebook
             </a>
-          </div>
-          <div className="contact-card">
+          </div> */}
+          {/* <div className="flex gap-4">
             <a
               href="https://github.com/lixinjie520"
               target="_blank"
-              className="link"
+              className="flex items-center"
             >
               <FaGithub className="mr-1"></FaGithub>
               Github
             </a>
           </div>
-          <div className="contact-card ">
+          <div className="flex gap-4">
             <a
               href="https://www.linkedin.com/in/%E5%8B%9D%E7%87%95-%E7%8E%8B-61b6b3286/"
               target="_blank"
-              className="link"
+              className="flex items-center"
             >
               <FaLinkedin className="mr-1"></FaLinkedin>
               LinkedIn
             </a>
-          </div>
-          <div className="contact-card ">
+          </div> */}
+          {/* <div className="contact-card ">
             <a href="mailto:wangshengyan6@gmail.com" className="link">
               <FaAt className="mr-1"></FaAt>
               Send a mail
             </a>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
